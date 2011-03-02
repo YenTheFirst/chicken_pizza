de94bbb2bd685e3315be787b1710ad96f534b70a
T S <YenTheFirst@gmail.com>
2011-03-02 14:54:56 -0800

    added and fixed bunches of  stuff

--- mpd_driver.rb
+++ mpd_driver.rb
@@ -7,12 +7,23 @@ class MPD
 	end
 	def all_tracks
 		puts "loading tracz"
-		list_all_info.slice_before(/^file/).map {|line| Hash[*line.map {|entry| entry.match(/([^:]*): (.*)/)[1,2]}.flatten]}  
+		list_all_info.slice_before(/^file/).map {|line| line_to_hash(line)}
 	end
 	def list_all_info
 		send_command("listallinfo")
 	end
-
+	def status
+		res=line_to_hash(send_command("status"))
+		res["queue_version"]=res.delete("playlist")
+		res["queue_length"]=res.delete("playlistlength")
+		res
+	end
+	def queue
+		send_command("playlistinfo").slice_before(/^file/).map {|line| line_to_hash(line)}
+	end
+	def line_to_hash(line)
+		Hash[*line.map {|entry| entry.match(/([^:]*): (.*)/)[1,2]}.flatten]
+	end
 	def send_command(cmd)
 		tcp=TCPSocket.new(@url,@port)
 		tcp.set_encoding("UTF-8")
@@ -20,10 +31,14 @@ class MPD
 		tcp.puts cmd
 		all=[]
 			#this is a little unsafe, it depends on the other end to close the connection
-		until all.last=="OK"
+		until all.last=="OK" or all.last =~ /^ACK/
 			all<< tcp.gets.chomp
 		end
 		tcp.close
-		all[0..-2]
+		if all.last=="OK"
+			all[0..-2] #pull off the ok
+		else
+			[all.last]
+		end
 	end
 end
--- pizza.rb
+++ pizza.rb
@@ -1,7 +1,14 @@
 #require 'mpd_driver.rb'
 load 'mpd_driver.rb'
 $mpd=MPD.new("chicken.local")
-
+#SECTION: other functions
+get '/' do
+	redirect '/index.html'
+end
+get '/status' do
+	$mpd.status.to_json
+end
+#SECTION: search functions
 def all_tracks
 	$cached_tracks ||= $mpd.all_tracks
 end
@@ -21,13 +28,76 @@ get '/search/:query' do
 		end
 	end.to_json
 end
+
 get '/filter' do
 	available={}
 	tracks=all_tracks.clone
+		#WARNING: this function only works *correctly* if the hash is in the order it was in when presented to the application.
+		#I don't know if this is standard behavior of get params, and if it is, how to guarantee it.
+		#it does, however, seem to work for the moment
 	params[:filters].each do |(tag,selected_choices)|
 		available[tag]=tracks.map {|entry| entry[tag]}.uniq
-		tracks.select! {|entry| selected_choices.include? entry[tag]}
+		if (selected_choices) # if they gave us no choices, just return everything.
+			if i=selected_choices.index("")
+				selected_choices[i]=nil #for unknown albums
+			end
+			tracks.select! {|entry| selected_choices.include? entry[tag]}
+		end
 	end
 	available["Tracks"]=tracks
 	available.to_json
+end
+#SECTION: 'queue' functions. MPD calls these 'playlist' commands, but I'm differentiating here the live queue from saved playlists.
+#all commands will return the current queue in JSON
+#NOTE: all these will have to add a bit of thread/version safety
+post '/queue/add' do
+	#add params[:filename] to the queue
+	#TODO: safety here
+	safe_name=params[:filename].gsub('"','\"')
+	res=$mpd.send_command("add \"#{safe_name}\"")
+	if res.empty?
+		'["ok"]'
+	else
+		res
+	end
+	#TODO: return
+end
+put '/queue/clear' do 
+	$mpd.send_command("clear")
+	#TODO: return
+	'["ok"]'
+end
+delete '/queue/delete/:song_id' do
+	#TODO: safety here
+	$mpd.send_command("delete "+params[:song_id])
+	#TODO: return
+end
+put '/queue/move/:song_id' do
+	$mpd.send_command("move #{params[:song_id]} #{params[:pos]}")
+end
+get '/queue/list' do
+	$mpd.queue.to_json
+end
+#SECTION: 'playlist' functions
+#SECTION: playback functions
+post '/playback/next' do
+	$mpd.send_command("next")
+	'["ok"]'
+end
+post '/playback/previous' do
+	$mpd.send_command("previous")
+	'["ok"]'
+end
+put '/playback/pause' do
+	#TODO: safety here
+	$mpd.send_command("pause #{params[:state]}")
+	'["ok"]'
+end
+put '/playback/play' do
+	$mpd.send_command("play #{params[:pos]}")
+	'["ok"]'
+end
+put '/playback/setvol' do
+	$mpd.send_command("setvol #{params[:vol]}")
+	'["ok"]'
 end
